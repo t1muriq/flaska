80636e35fbf6bf004d68660f824e8064af5ddea0
timur <ramazanov.timyr-ramazanov@yandex.ru>
2025-04-02 12:11:13 +0000

    laptop

--- models.py
+++ models.py
@@ -1,7 +1,7 @@
 # models.py
 
 from datetime import datetime
-from config import db
+from config import db, ma
 
 class Person(db.Model):
     __tablename__ = "person"
@@ -11,3 +11,12 @@ class Person(db.Model):
     timestamp = db.Column(
         db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow
     )
+
+class PersonSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = Person
+        load_instance = True
+        sqla_session = db.session
+
+person_schema = PersonSchema()
+people_schema = PersonSchema(many=True)
--- people.py
+++ people.py
@@ -1,28 +1,9 @@
 # people.py
 
-from datetime import datetime
 from flask import abort, make_response
+from config import db
+from models import Person, people_schema, person_schema
 
-def get_timestamp():
-    return datetime.now().strftime(("%Y-%m-%d %H:%M:%S"))
-
-PEOPLE = {
-    "Fairy": {
-        "fname": "Tooth",
-        "lname": "Fairy",
-        "timestamp": get_timestamp(),
-    },
-    "Ruprecht": {
-        "fname": "Knecht",
-        "lname": "Ruprecht",
-        "timestamp": get_timestamp(),
-    },
-    "Bunny": {
-        "fname": "Easter",
-        "lname": "Bunny",
-        "timestamp": get_timestamp(),
-    }
-}
 
 def read_all():
     return list(PEOPLE.values())
