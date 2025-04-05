2d2ab5fa2e378bc7719bf56e665033e99d876818
timur <ramazanov.timyr-ramazanov@yandex.ru>
2025-04-05 09:29:22 +0000

    notebooktim

--- people.py
+++ people.py
@@ -6,32 +6,31 @@ from models import Person, people_schema, person_schema
 
 
 def read_all():
-    return list(PEOPLE.values())
+    people = Person.query.all()
+    return people_schema.dump(people)
 
 def create(person):
-    lname = person.get("lname")
-    fname = person.get("fname", "")
+ lname = person.get("lname")
+    existing_person = Person.query.filter(Person.lname == lname).one_or_none()
 
-    if lname and lname not in PEOPLE:
-        PEOPLE[lname] = {
-            "lname": lname,
-            "fname": fname,
-            "timestamp": get_timestamp(),
-        }
-        return PEOPLE[lname], 201
-    else:
+    if existing_person is None:
+        new_person = person_schema.load(person, session=db.session)
+        db.session.add(new_person)
+        db.session.commit()
+        return person_schema.dump(new_person), 201
+else:
         abort(
             406,
             f"Person with last name {lname} already exists",
         )
 
 def read_one(lname):
-    if lname in PEOPLE:
-        return PEOPLE[lname]
+    person = Person.query.filter(Person.lname == lname).one_or_none()
+
+    if person is not None:
+        return person_schema.dump(person)
     else:
-        abort(
-            404, f"Person with last name {lname} not found"
-        )
+        abort(404, f"Person with last name {lname} not found")
 
 def update(lname, person):
     if lname in PEOPLE:
